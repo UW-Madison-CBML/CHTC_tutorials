788cf926b136f5c979411a6c440cfe35fbf996fb
lmsvaren <149616682+lmsvaren@users.noreply.github.com>
2026-04-02 20:29:13 -0500

    Update tutorial 1

--- tutorial_1/run_script.sh
+++ tutorial_1/run_script.sh
@@ -6,27 +6,29 @@ pip install tifffile
 pip install torchvision
 pip install torch-topological
 pip install pillow
+pip install matplotlib
+pip install seaborn
 
-
-##### Add any needed 
+# Unzip input image files
 echo "unzip"
-tar -zxf google_birds.tar.gz
+tar -zxf tutorial_1_inputs.tar.gz
 
-echo "move to one directory"
-
+# Make necessary directories
 mkdir tutorial_1_outputs
 mkdir patches
 
 echo "Running"
 
+# Assign variables
 img_path='./tutorial_1_inputs/'
 output_dir='./tutorial_1_outputs/'
 n_epochs=50
 save_name='tutorial_1_inputs_model'
 
-python model.py $img_path $output_dir $n_epochs $save_name
+# Run script using variables
+python train_model.py $img_path $output_dir $n_epochs $save_name
 
-
+# tar.gz the output directory
 tar -zcvf tutorial_1_outputs.tar.gz tutorial_1_outputs
 
 echo "DONE"
