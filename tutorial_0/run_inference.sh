55e3fd039339242212680a0ee1b1c7fbf8556bf7
lmsvaren <149616682+lmsvaren@users.noreply.github.com>
2026-04-07 15:00:21 -0500

    tutorial 0

---
+++ tutorial_0/run_inference.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+##### Additional packages needed not in your docker container yet
+pip install torch==2.10.0
+pip install torchvision==0.25.0
+pip install tifffile
+pip install pillow
+pip install matplotlib
+pip install seaborn
+
+# Unzip input image files
+echo "unzip"
+tar -zxf tutorial_1_inputs.tar.gz
+
+# Make necessary directories
+mkdir tutorial_1_outputs
+mkdir patches
+
+echo "Running"
+
+# Assign variables
+img_path='./tutorial_1_inputs/'
+output_dir='./tutorial_1_outputs/'
+n_epochs=50
+save_name='tutorial_1_inputs_model'
+
+# Run script using variables
+python train_model.py $img_path $output_dir $n_epochs $save_name
+
+# tar.gz the output directory
+tar -zcvf tutorial_1_outputs.tar.gz tutorial_1_outputs
+
+echo "DONE"
